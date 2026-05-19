cf808c2861edb395845343b4b52403965897ee39
murali <murali.pydi@gmail.com>
2026-05-19 22:03:45 +0530

    code change in files

--- mysql/custom-script.sh
+++ mysql/custom-script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-if [ -f /tmp/mysql-root-password.txt ]; then
-    PASSWORD=$(cat /tmp/mysql-root-password.txt)
-    echo "Accessed MySQL root password"
-else
-    echo "MySQL Root password file not found"
-    exit 1
-fi
+export MYSQL_ROOT_PASSWORD=RoboShop@1
 
-export MYSQL_ROOT_PASSWORD=$PASSWORD
-rm -rf /tmp/mysql-root-password.txt
 exec /entrypoint.sh mysqld
+
+
+# if [ -f /tmp/mysql-root-password.txt ]; then
+#     PASSWORD=$(cat /tmp/mysql-root-password.txt)
+#     echo "Accessed MySQL root password"
+# else
+#     echo "MySQL Root password file not found"
+#     exit 1
+# fi
+
+# export MYSQL_ROOT_PASSWORD=$PASSWORD
+# rm -rf /tmp/mysql-root-password.txt
+# exec /entrypoint.sh mysqld
